0b76c5e1baa1ac3e2e54cf67d5b621958d0c2b7e
Artaniel <59449523+Artaniel@users.noreply.github.com>
2023-04-16 20:30:54 +0300

    input without input system

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -11,21 +11,39 @@ public class PlayerController : MonoBehaviour
     public bool movementEnabled = true;
     public float sensitivityX = 0.1f;
     public float sensitivityY = 0.1f;
-    private float verticalRotetion = 0f;
+    private float verticalRotation = 0f;
 
-    public InputActionAsset actions;
+    public float acceleration = 10f;
+    public float maxGroundSpeed = 2f;
+    private Vector2 lastmoveInput = Vector2.zero;
 
     private void Update()
     {        
         playerTransform.Rotate(Vector3.up, Mouse.current.delta.value.x * sensitivityX);
-        verticalRotetion = Mathf.Clamp(verticalRotetion - Mouse.current.delta.value.y * sensitivityY, -90f, 90f);
-        cameraTransform.localRotation = Quaternion.Euler(verticalRotetion, 0, 0);
+        verticalRotation = Mathf.Clamp(verticalRotation - Mouse.current.delta.value.y * sensitivityY, -90f, 90f);
+        cameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
 
+        Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
         if (movementEnabled)
         {
-            Vector2 moveInput = actions.FindActionMap("Player").FindAction("move").ReadValue<Vector2>();
-            Debug.Log(actions.FindActionMap("Player").FindAction("move"));
-            playerRigidbody.AddForce(new Vector3(moveInput.y, moveInput.x, 0), ForceMode.VelocityChange);
+            if (moveInput.magnitude < lastmoveInput.magnitude && Vector3.Angle(moveInput, lastmoveInput) < 1f)
+            {
+                lastmoveInput = moveInput;
+                moveInput = Vector2.zero;
+            }else
+                lastmoveInput = moveInput;
+            Debug.Log(moveInput);
+            if (moveInput != Vector2.zero)
+            {
+                Vector3 velocity = playerRigidbody.velocity;
+                velocity += (moveInput.y * playerTransform.forward + moveInput.x * playerTransform.right) * Time.deltaTime * acceleration;
+                if (velocity.magnitude >= maxGroundSpeed)
+                    velocity = velocity.normalized * maxGroundSpeed;
+                playerRigidbody.velocity = velocity;
+            }
+            else {
+                playerRigidbody.velocity = Vector3.zero;
+            }
         }
     }
 }
