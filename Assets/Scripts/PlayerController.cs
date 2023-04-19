d22c821fe1765c213324154859f34e0d3aec42ca
Artaniel <59449523+Artaniel@users.noreply.github.com>
2023-04-19 13:18:05 +0300

    camera shake on runing

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -14,24 +14,39 @@ public class PlayerController : MonoBehaviour
     public float sensitivityY = 0.1f;
     private float verticalRotation = 0f;
 
-    public float acceleration = 10f;
-    public float maxGroundSpeed = 2f;
-    private Vector2 lastmoveInput = Vector2.zero;
+    public float speed = 10f;
 
     public Transform cameraDefaultPivot;
+    private float inMoveTime = 0f;
 
     private void Update()
     {
         if (movementEnabled)
         {
-            playerTransform.Rotate(Vector3.up, Mouse.current.delta.value.x * sensitivityX);
-            verticalRotation = Mathf.Clamp(verticalRotation - Mouse.current.delta.value.y * sensitivityY, -90f, 90f);
+            playerTransform.Rotate(Vector3.up, Mouse.current.delta.value.x * sensitivityX * Time.deltaTime);
+            verticalRotation = Mathf.Clamp(verticalRotation - Mouse.current.delta.value.y * sensitivityY * Time.deltaTime, -90f, 90f);
             cameraTransform.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
 
             Vector2 moveInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
 
-            Vector3 velocity = (moveInput.y * playerTransform.forward + moveInput.x * playerTransform.right) * Time.deltaTime * acceleration;
+            Vector3 velocity = (moveInput.y * playerTransform.forward + moveInput.x * playerTransform.right) * Time.deltaTime * speed;
             characterController.Move(velocity);
+
+
+            if (moveInput != Vector2.zero)
+                inMoveTime += Time.deltaTime;
+            else inMoveTime = 0;
+            CamraShift();
         }
+    }
+
+    private void CamraShift()
+    {
+        float stepTime = 0.5f;
+        float cameraShakeAmp = 0.5f;
+        float phase = inMoveTime / stepTime;
+        phase -= (int)phase;
+        Vector3 cameraTargetPosition = cameraDefaultPivot.position + Vector3.up * cameraShakeAmp * phase;
+        cameraTransform.position = Vector3.Lerp(cameraTransform.position, cameraTargetPosition, 0.05f);
     }
 }
