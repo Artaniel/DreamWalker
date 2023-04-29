b1d1829f1624dc576c6a9c9ee06dcbd3ed13f601
Artaniel <59449523+Artaniel@users.noreply.github.com>
2023-04-29 23:05:35 +0300

    turning

--- Assets/Scripts/WallCar/WallCar.cs
+++ Assets/Scripts/WallCar/WallCar.cs
@@ -1,20 +1,34 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class WallCar : MonoBehaviour
 {
+    //bug задний ход стопорится в ноль и дрожит
+    // проверить Debug.Ray 
+
     public Transform[] groundCheckPoints;
     private bool isOnSurface = false;
+    private float speed = 0f;
+    public float maxSpeed = 5f;
+    public float acceleration = 2f;
+    private float verticalRotation = 0f;
+    public float senetivity = 1f;
+    public Transform cameraHolder;
+    private Rigidbody carRigidbody;
 
     private void Awake()
     {
-        
+        carRigidbody = GetComponent<Rigidbody>();
     }
 
     private void Update()
     {
         SurfaceCheck();
+        if (isOnSurface)
+            Move();
+        MouseTurn();
     }
 
     private void SurfaceCheck() {
@@ -27,10 +41,30 @@ public class WallCar : MonoBehaviour
             {
                 touchPointFound = true;
                 connectionNormalSumm += hit.normal;
-                Debug.DrawRay(groundCheckPoint.position, - groundCheckPoint.up, Color.green);
+               
+            }
+            else {
+                Debug.DrawRay(groundCheckPoint.position, -groundCheckPoint.up, Color.red);
             }
         }
         isOnSurface = touchPointFound;
         
+    }
+
+    private void Move() {
+        if (Keyboard.current.wKey.isPressed)
+            speed = Mathf.Clamp(speed + acceleration * Time.deltaTime, -maxSpeed, maxSpeed);
+        else if (Keyboard.current.sKey.isPressed)
+            speed = Mathf.Clamp(- speed + acceleration * Time.deltaTime, -maxSpeed, maxSpeed);
+        else
+            speed = Mathf.Clamp(speed - acceleration * Time.deltaTime, 0, maxSpeed);
+        //transform.position += transform.forward * speed * Time.deltaTime;
+        carRigidbody.velocity = transform.forward * speed;
+    }
+
+    private void MouseTurn() {
+        transform.rotation = Quaternion.Euler(0, transform.rotation.eulerAngles.y + Mouse.current.delta.value.x * senetivity * Time.deltaTime, 0);
+        verticalRotation = Mathf.Clamp(verticalRotation - Mouse.current.delta.value.y * senetivity * Time.deltaTime, 0f, 90f);
+        cameraHolder.localRotation = Quaternion.Euler(verticalRotation, 0, 0);
     }
 }
