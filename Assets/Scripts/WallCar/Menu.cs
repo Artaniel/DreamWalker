0ede077e058861d4faea5ccd91e468ab4d73a071
Artaniel <59449523+Artaniel@users.noreply.github.com>
2023-06-11 11:56:11 +0300

    main menu settings

--- Assets/Scripts/WallCar/Menu.cs
+++ Assets/Scripts/WallCar/Menu.cs
@@ -2,18 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+using UnityEngine.UI;
 
 public class Menu : MonoBehaviour
 {
     public string newGameSceneName = "";
     public string mainMenuSceneName = "";
+    public GameObject settingsPanel;
+    public GameObject mainMenuButton;
+    public GameObject quitButton;
 
     public void NewGameButton() {
         SceneManager.LoadScene(newGameSceneName);
-    }
-
-    public void SettingsButton() { 
-        //todo
     }
 
     public void ExitButton()
@@ -23,5 +24,32 @@ public class Menu : MonoBehaviour
 
     public void MainMenuButton() {
         SceneManager.LoadScene(mainMenuSceneName);
+    }
+
+    public void OpenMainMenuSettings() {
+        settingsPanel.SetActive(true);
+        quitButton.SetActive(false);
+        mainMenuButton.SetActive(false);
+    }
+
+    public void OpenIngameSettings()
+    {
+        settingsPanel.SetActive(true);
+        quitButton.SetActive(true);
+        mainMenuButton.SetActive(true);
+    }
+
+    public void CloseSettings()
+    {
+        settingsPanel.SetActive(false);
+    }
+
+    public void SoundSliderInput(Slider slider) { 
+    //todo, when we will have sound system
+    }
+
+    public void MusicSliderInput(Slider slider)
+    {
+        //todo, when we will have sound system
     }
 }
