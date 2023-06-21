29b3d62f37ac1b71479a8ffb11dc229177070860
TripletConcept <45384713+TripletConcept@users.noreply.github.com>
2023-06-21 18:20:41 +0300

    Cutscene camera movement script

---
+++ Assets/Scripts/Cutscenes/CutsceneCamera.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CutsceneCamera : MonoBehaviour
+{
+    public float cameraLerpFactor, mouseSensitivity;
+    public Transform zeroPoint;
+
+    void Start()
+    {
+        Cursor.visible = false;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        Vector2 screenDimensions = new Vector2(Screen.width, Screen.height);
+        Vector2 mouseRelativePos = mousePos - screenDimensions / 2;
+        Vector3 mouseAdjustedPos = new Vector3(mouseRelativePos.x, mouseRelativePos.y, 0) * mouseSensitivity;
+        Vector3 currentTarget = mouseAdjustedPos + zeroPoint.position;
+        Vector3 newCameraPosition = new Vector3(Mathf.Lerp(transform.position.x, currentTarget.x, cameraLerpFactor), Mathf.Lerp(transform.position.y, currentTarget.y, cameraLerpFactor), transform.position.z);
+        transform.position = newCameraPosition;
+    }
+
+}
